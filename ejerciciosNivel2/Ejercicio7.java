f1344c92fbc60dfde7cdd7f561c75c5ff4677237
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2022-05-24 11:34:41 -0300

    completo ejercicios nivel 2

--- ejerciciosNivel2/Ejercicio7.java
+++ ejerciciosNivel2/Ejercicio7.java
@@ -1,3 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+Crear una función que dado 2 argumentos (int, siendo el primero menor al segundo), nos devuelva un array de Strings. Con la secuencia de números enteros de principio a final. Pero si el número es multiplo de 2 colocara el valor “Fizz”, si es múltiplo de 3 “Buzz” y si es a la vez múltiplo de ambos colocara “FizzBuzz”. 
+Observacion: Los 2 argumentos indican con que valor se arranca a calcular y el segundo con qué valor debe frenar (no se incluye en el cálculo)
+Ejemplo: (1, 5) ----> calculará valores de 1, 2, 3, 4 
+
+
+*/
+
 public class Ejercicio7 {
-    
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese numero de incio: ");
+        int numInicio = scan.nextInt();
+        System.out.println("Ingrese numero final: ");
+        int numFinal = scan.nextInt();
+        scan.close();
+
+        ArrayList<String> lista = new ArrayList<>();
+        for (int i = numInicio; i < numFinal; i++){
+            if (i % 2 == 0 && i % 3 == 0){
+                lista.add("FizzBuzz");
+            } else if (i % 2 == 0){
+                lista.add("Fizz");
+            } else if (i % 3 == 0){
+                lista.add("Buzz");
+            } else {
+                lista.add(String.valueOf(i));
+            }
+        }
+        System.out.println(lista);
+    }
 }
